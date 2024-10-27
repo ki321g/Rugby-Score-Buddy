24b8e9df2e8a849d7a11d548205b7781e1d70f8f
Kieron Garvey <garveykieron@gmail.com>
2024-10-27 20:52:25 +0000

    Added Splash Screen, Loading Screen

--- app/src/main/java/org/setu/rugbyscorebuddy/activities/SplashScreenActivity.kt
+++ app/src/main/java/org/setu/rugbyscorebuddy/activities/SplashScreenActivity.kt
@@ -1,21 +1,83 @@
 package org.setu.rugbyscorebuddy.activities
 
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import android.view.animation.OvershootInterpolator
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.setu.rugbyscorebuddy.R
+import org.setu.rugbyscorebuddy.models.RugbyScoreViewModel
 
 class SplashScreenActivity : AppCompatActivity() {
+    //lateinit var app: MainApp
+    private val viewModel by viewModels<RugbyScoreViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Working with splashScreen
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+            // Set Exit of the splash screen animation
+            setOnExitAnimationListener { screen ->
+                // Zoom Out the X Value
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.8f,
+                    0.0f
+                )
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                // Zoom Out the Y Value
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.8f,
+                    0.0f
+                )
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
         }
+
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide() // Hide the ActionBar
+        // Change ActionBar color
+//        supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimaryDark)))
+
+        val goToRugbyscore = findViewById<Button>(R.id.loginButton)
+
+        goToRugbyscore.setOnClickListener {
+            // Navigate to LoginActivity
+            val intent = Intent(this, RugbyScoreListActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+//        signupButton.setOnClickListener {
+//            // Navigate to SignupActivity
+//            startActivity(Intent(this, SignupActivity::class.java))
+//        }
+
     }
 }
--- app/src/main/java/org/setu/rugbyscorebuddy/models/RugbyScoreViewModel.kt
+++ app/src/main/java/org/setu/rugbyscorebuddy/models/RugbyScoreViewModel.kt
@@ -1,4 +1,21 @@
 package org.setu.rugbyscorebuddy.models
 
-class RugbyScoreViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RugbyScoreViewModel: ViewModel() {
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            //Can check if user is authenticated here
+            delay(3000L)
+            _isReady.value = true
+        }
+    }
 }
